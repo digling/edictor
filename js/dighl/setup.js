2d5dee22a2b6abc214ca3ba5e7d18b9201d3cc1e
LinguList <mattis.list@posteo.de>
2014-09-04 17:43:03 +0200

    new schema for wordlist editor using prison.js as basic library

--- js/dighl/setup.js
+++
@@ -1,7 +0,0 @@
-loadModule('dighl/vendor/jquery.js');
-loadModule('dighl/vendor/jquery-ui.js');
-loadModule('dighl/utils.js');
-loadModule('dighl/sampa.js');
-loadModule('dighl/edit.js');
-loadModule('dighl/highlight.js');
-
