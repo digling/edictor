a3fc571cfab7dd4579b7dc5e01f986380e3ea8d9
lingulist <mattis.list@lingulist.de>
2024-07-30 22:10:24 +0200

    add test for server

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,43 @@
 from edictor.cli import main
 import shlex
+import multiprocessing
+import time
+import os
+import tempfile
+from pathlib import Path
+
+#def run(capsys, *args):
+#    if len(args) == 1:
+#        args = shlex.split(args[0])
+#    main(*args)
+#    return capsys.readouterr().out
 
 
-def run(capsys, *args):
-    if len(args) == 1:
-        args = shlex.split(args[0])
-    main(*args)
-    return capsys.readouterr().out
+def test_server():
+    p = multiprocessing.Process(target=main, args=('server', '--browser=dummy'))
+    p.start()
+    time.sleep(1)
+    p.kill()
 
 
-def test_server(capsys):
-    pass
+def test_fetch():
+
+    with tempfile.TemporaryDirectory() as t:
+        os.system("edictor fetch --dataset=sumerian --name=" + t +
+                  "/dummy.tsv")
+
+
+def test_wordlist():
+    with tempfile.TemporaryDirectory() as t:
+        os.chdir(t)
+        os.system("git clone https://github.com/lexibank/allenbai")
+        os.system("edictor wordlist --dataset=allenbai/cldf/cldf-dataset.json")
+        os.chdir("..")
+
+
+def test_main():
+    os.system("edictor")
+    os.system("edictor --help")
 
 #output = run(
 #            capsys, "server")
